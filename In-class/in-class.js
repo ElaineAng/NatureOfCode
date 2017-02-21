53521eb6410692698be5e1f6994e7967f290a112
Elaine <ra1695@nyu.edu>
2017-02-21 16:23:22 +0800

    js weird vector

--- In-class/in-class.js
+++ In-class/in-class.js
@@ -1,7 +1,19 @@
 function setup(){
+  createCanvas(600, 600);
+
+  var vector1 = createVector(10, 5);
+  var vector2 = createVector(2, 3);
+  //var newVector = createVector(0, 0);
+  var newVector = p5.Vector.add(vector1, vector2)
+
+  // surprise!!!
+  // newVector = vector1.add(vector)
+  // newVector.x = -5
+  // console.log (vector1.x)
+  // Yahhhh its -5 ... - -
 
 }
 
 function draw(){
-  
+  noLoop();
 }
