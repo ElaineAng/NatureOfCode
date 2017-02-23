f00b640b14bffb8161bf82a30d53299a4b2dab81
Elaine <ra1695@nyu.edu>
2017-02-23 16:10:21 +0800

    vector and velocity 1

--- In-class/in-class.js
+++ In-class/in-class.js
@@ -1,21 +1,48 @@
+var pos, vel;
+var centerVec, mouseVec;
+
 function setup(){
   createCanvas(600, 600);
-
+  background(0);
+  // x = width / 2;
+  // y = height / 2;
+  // x_spd = 1;
+  // y_spd = 1;
+  pos = createVector(width/2, height/2);
+  centerVec = createVector(width/2, height/2);
+  vel = createVector(1, 1);
 }
 
 function draw(){
-  background(0)
-
-  var vector1 = createVector(width/2, height/2);
-  var vector2 = createVector(mouseX, mouseY);
-
-  var newVector = p5.Vector.sub(vector2, vector1);
-  //console.log( newVector.mag());
-
-  ellipse(vector1.x, vector1.y, 10, 10);
-  ellipse(vector2.x, vector2.y, 10, 10);
+  background(0);
+  mouseVec = createVector(mouseX, mouseY);
+  vel = p5.Vector.sub(mouseVec, centerVec);
+  //vel.limit(50);
+  //vel.setMag(50);
+  vel.normalize(); //just to get the direction
+  vel.mult(50);
+  pos.add(vel);
+  checkBoundaries();
+  noStroke();
+  ellipse(pos.x, pos.y, 30, 30);
 
   translate(width/2, height/2);
-  stroke(255);
-  line(0, 0, newVector.x, newVector.y);
+  noStroke(255);
+  line(0, 0, vel.x, vel.y);
+  fill(255);
+  text(round(vel.mag()), vel.x+15, vel.y);
+}
+
+function checkBoundaries(){
+  if (pos.x < 0){
+    pos.x = width;
+  } else if (pos.x > width){
+    pos.x = 0;
+  }
+
+  if (pos.y < 0){
+    pos.y = height;
+  } else if (pos.y > height){
+    pos.y = 0;
+  }
 }
