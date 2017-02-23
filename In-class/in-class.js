fedca42b9249ea2b50483f11f636c0edd4c700de
Elaine <ra1695@nyu.edu>
2017-02-23 16:27:18 +0800

    circle shape with from Angle

--- In-class/in-class.js
+++ In-class/in-class.js
@@ -3,46 +3,25 @@ var centerVec, mouseVec;
 
 function setup(){
   createCanvas(600, 600);
-  background(0);
-  // x = width / 2;
-  // y = height / 2;
-  // x_spd = 1;
-  // y_spd = 1;
-  pos = createVector(width/2, height/2);
   centerVec = createVector(width/2, height/2);
-  vel = createVector(1, 1);
+  background(0);
 }
 
 function draw(){
-  background(0);
-  mouseVec = createVector(mouseX, mouseY);
-  vel = p5.Vector.sub(mouseVec, centerVec);
-  //vel.limit(50);
-  //vel.setMag(50);
-  vel.normalize(); //just to get the direction
-  vel.mult(50);
-  pos.add(vel);
-  checkBoundaries();
-  noStroke();
-  ellipse(pos.x, pos.y, 30, 30);
-
+  //background(0);
   translate(width/2, height/2);
-  noStroke(255);
-  line(0, 0, vel.x, vel.y);
+  mouseVec = createVector(random(100, 200), random(200, 400));
+  var vector = p5.Vector.sub(mouseVec, centerVec);
+
+  var vecFromAngle = p5.Vector.fromAngle(random(PI))
+  vecFromAngle.mult(random(100, 300));
+
+
+  stroke(255);
+  line(0, 0, vecFromAngle.x, vecFromAngle.y);
+  //line(0, 0, vector.x, vector.y);
+
+  noStroke();
   fill(255);
-  text(round(vel.mag()), vel.x+15, vel.y);
-}
-
-function checkBoundaries(){
-  if (pos.x < 0){
-    pos.x = width;
-  } else if (pos.x > width){
-    pos.x = 0;
-  }
-
-  if (pos.y < 0){
-    pos.y = height;
-  } else if (pos.y > height){
-    pos.y = 0;
-  }
+  ellipse(vecFromAngle.x, vecFromAngle.y, 10, 10);
 }
