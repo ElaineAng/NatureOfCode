d686bd6707a72f65a89b20c13c882e481e69b247
Elaine <ra1695@nyu.edu>
2017-03-12 23:05:00 +0800

    collision sample

--- In-class/Particle.js
+++ In-class/Particle.js
@@ -1,77 +1,70 @@
 "use strict";
 
+const C_GRAVITY = 5;
 
-
-class Particle{
-  constructor(x, y, r, m){
-    this.hit = false;
+class Particle {
+  constructor(x, y, m, vx, vy) {
     this.pos = createVector(x, y);
-    this.vel = createVector(0, 0);
+    this.vel = createVector(vx, vy);
     this.acc = createVector(0, 0);
-    this.r = r;
     this.mass = m;
+    this.rad = 3 * this.mass;
+    this.color = color(255);
   }
-
-  setSpeed(x, y){
-    this.vel.x = x;
-    this.vel.y = y;
-  }
-
-  checkEdges(){
-    if (this.pos.x < 0){
-      this.pos.x = width;
-    } else if (this.pos.x > width){
-      this.pos.x = 0;
-    }
-
-    if (this.pos.y < 0){
-      this.pos.y = height;
-    }else if (this.pos.y > height){
-      this.pos.y = 0;
-    }
-  }
-
-  update(){
-    this.vel.add(this.acc);
-    this.pos.add(this.vel);
-    this.acc.mult(0);
-  }
-
-  applyForce(f){
+  applyForce(f) {
     f.div(this.mass);
     this.acc.add(f);
   }
 
-  applyAttraction(other){
+  applyAttraction(other) {
+    var distance = this.pos.dist(other.pos); // distance
+    var magnitude = (C_GRAVITY * this.mass * other.mass)/(distance * distance); // gravity
     var force = p5.Vector.sub(other.pos, this.pos);
-    var distance = this.pos.dist(other.pos);
-    var G = 9.8;
     force.normalize();
-    force.mult(G * this.mass * other.mass / (distance * distance));
-    this.acc.add(force);
-  }
+    force.mult(magnitude);
+    this.applyForce(force);
+}
 
-  detectBoundary(other){
-    var dist = this.pos.dist(other.pos);
-    if (dist < (this.r * this.mass/2 + other.r*other.mass/2)){
-      this.hit = true;
-      this.vel = createVector(0, 0);
-      this.acc = createVector(0, 0);
-      other.vel = createVector(0, 0);
-      other.acc = createVector(0, 0);
+  checkCollision(other) {
+    var distance = this.pos.dist(other.pos);
+    var force;
+    if (distance < this.rad + other.rad){
+      force = p5.Vector.sub(other.pos, this.pos);
+
+      // other.vel.mult(0);
+      this.applyForce(force.mult(-1));
+      this.vel.mult(0);
+      //other.applyForce(force.mult(-1));
     }
 
   }
-  display(){
+  checkEdges() {
+    if (this.pos.x < 0) {
+      this.pos.x = 0;
+      this.vel.x = -this.vel.x;
+    } else if (this.pos.x > width) {
+      this.pos.x = width;
+      this.vel.x = -this.vel.x;
+    }
+    if (this.pos.y < 0) {
+      this.pos.y = 0;
+      this.vel.y = -this.vel.y;
+    } else if (this.pos.y > height) {
+      this.pos.y = height;
+      this.vel.y = -this.vel.y;
+    }
+  }
+  update() {
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.acc.mult(0);
+  }
+  display() {
     push();
-      translate(this.pos.x, this.pos.y);
-      fill(255, 100)
-      stroke(255);
-      ellipse(0, 0, this.r * this.mass, this.r*this.mass);
-      // if (DEBUG_MODE){
-      //   stroke(255, 0, 0);
-      //   line(0, 0, this.vel.x, this.vel.y);
-      // }
+    translate(this.pos.x, this.pos.y);
+    noStroke();
+    fill(255, 100);
+    ellipse(0, 0, this.rad * 2, this.rad * 2);
     pop();
   }
 }
--- In-class/sketch.js
+++ In-class/sketch.js
@@ -1,37 +1,36 @@
-"use strict";
+var particles = [];
 
-
-var p1, p2;
 
 function setup() {
-  createCanvas(500, 600);
+  createCanvas(800, 500);
   background(0);
 
-  p1 = new Particle(100, height/2, 5, 5);
-  p2 = new Particle(width/2, height/2, 5, 15);
-  p1.setSpeed(10, 0);
+  // particles.push(new Particle(100, height / 2, random(1, 15))); // (x,y,mass)
+  // particles.push(new Particle(700, height / 2, random(1, 15))); // (x,y,mass)
+  // particles[0].vel.x = random(1,5);
+  // particles[1].vel.x = -random(1,5);
+
+  for (var i = 0; i < 5; i++){
+    particles.push(new Particle(random(width), random(height), random(1,10), i, 0));
+  }
 }
+
 
 function draw() {
   background(0);
 
-  p1.checkEdges();
-  if (!p1.hit){
-    p1.applyAttraction(p2);
+  for (var a = 0; a < particles.length; a++) {
+    for (var b = 0; b < particles.length; b++){
+      if (b != a){
+        particles[a].applyAttraction(particles[b]);
+        particles[a].checkCollision(particles[b]);
+      }
+    }
+
+    // check the collision!
+
+    particles[a].update();
+    particles[a].checkEdges();
+    particles[a].display();
   }
-
-  p1.update();
-  p1.display();
-
-  p2.checkEdges();
-  if (!p2.hit){
-    p2.applyAttraction(p1);
-  }
-
-  p2.update();
-  p2.display();
-
-  p1.detectBoundary(p2);
-  p2.detectBoundary(p1);
-
 }
