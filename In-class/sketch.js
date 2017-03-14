9e43f111d160def8e8d950b4d90b4a9d8dc1145b
Elaine <ra1695@nyu.edu>
2017-03-15 00:59:47 +0800

    sin and cos

--- In-class/sketch.js
+++ In-class/sketch.js
@@ -1,36 +1,23 @@
-var particles = [];
-
-
 function setup() {
-  createCanvas(800, 500);
+  createCanvas(500, 600);
   background(0);
-
-  // particles.push(new Particle(100, height / 2, random(1, 15))); // (x,y,mass)
-  // particles.push(new Particle(700, height / 2, random(1, 15))); // (x,y,mass)
-  // particles[0].vel.x = random(1,5);
-  // particles[1].vel.x = -random(1,5);
-
-  for (var i = 0; i < 5; i++){
-    particles.push(new Particle(random(width), random(height), random(1,10), i, 0));
-  }
 }
 
 
 function draw() {
   background(0);
+  drawCircle(-frameCount*2, 100);
+}
 
-  for (var a = 0; a < particles.length; a++) {
-    for (var b = 0; b < particles.length; b++){
-      if (b != a){
-        particles[a].applyAttraction(particles[b]);
-        particles[a].checkCollision(particles[b]);
-      }
-    }
+function drawCircle(angle, distance){
+  var x = cos(radians(angle)) * distance;
+  var y = cos(radians(angle)) * distance;
+  // var vector = p5.Vector.fromAngle(radians(angle));
+  // vector.mult(distance);
 
-    // check the collision!
-
-    particles[a].update();
-    particles[a].checkEdges();
-    particles[a].display();
-  }
+  push();
+  translate(width/2, height/2);
+  rotate(radians(angle));
+  ellipse(vector.x, vector.y, 50, 50);
+  pop();
 }
