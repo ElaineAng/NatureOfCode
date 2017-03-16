e45d1f8cd04b88357291e6ab28aa48377f1245a0
Elaine <ra1695@nyu.edu>
2017-03-16 15:50:56 +0800

    triangle rotate with mouse

--- In-class/sketch.js
+++ In-class/sketch.js
@@ -6,18 +6,22 @@ function setup() {
 
 function draw() {
   background(0);
-  drawCircle(-frameCount*2, 100);
-}
+  var centerVector = createVector(width/2, height/2);
+  var mouseVector = createVector(mouseX, mouseY);
+  stroke(255);
+  line (centerVector.x, centerVector.y, mouseVector.x, mouseVector.y);
 
-function drawCircle(angle, distance){
-  var x = cos(radians(angle)) * distance;
-  var y = cos(radians(angle)) * distance;
-  // var vector = p5.Vector.fromAngle(radians(angle));
-  // vector.mult(distance);
+  var vector = p5.Vector.sub(mouseVector, centerVector);
+  var angle = vector.heading();
 
-  push();
+  fill(255);
+  noStroke();
+  text(round(degrees(angle)), mouseVector.x, mouseVector.y);
+
+
   translate(width/2, height/2);
-  rotate(radians(angle));
-  ellipse(vector.x, vector.y, 50, 50);
-  pop();
+  rotate(angle);
+  fill(255, 0, 0);
+  noStroke();
+  triangle(0, 0, -30, -10, -30, 10);
 }
