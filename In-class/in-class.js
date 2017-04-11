cf4099433babc9d61808301fe32b588d58acfabf
Elaine <ra1695@nyu.edu>
2017-04-11 19:40:37 +0800

    exploding balls, with memory optimization

--- In-class/in-class.js
+++ In-class/in-class.js
@@ -1,34 +1,32 @@
-var gui = new dat.gui.GUI();
-
-var params = {
-  debugMode : false,
-  diameter: 200,
-  vibrationAmp: 5
-}
-
-gui.add(params, "debugMode");
-gui.add(params, "diameter").min(100).max(300).step(10);
-gui.add(params, "vibrationAmp", 5, 10, 1); //same as above line; 2 interfaces
+"use strict";
+const PAD = 50;
+var particles = [];
 
 function setup(){
   createCanvas(500, 600);
-  background(0);
 }
 
 function draw(){
-  background(0, 5);
 
-  va = params.vibrationAmp;
-  dia = params.diameter;
-  if (!params.debugMode){
-    fill(255, 150);
-    noStroke();
-  } else{
-    stroke(255);
-    noFill();
+  background(0);
+  particles.push(new Particle(width/2, height/2));
+
+  for (var i=0; i<particles.length; i++){
+    var p = particles[i];
+    p.update();
+    p.display();
+    p.checkOutOfCanvas();
+    if (p.isDone){
+      particles.splice(i, 1);
+    }
   }
 
-  var vX = random(-va, va) + width/2;
-  var vY = random(-va, va) + height/2;
-  ellipse(vX, vY, dia, dia);
+  if (particles.length > 300){
+    particles.splice(0, 1);
+  }
+  stroke(255, 0, 0);
+  line(PAD, PAD, width-PAD, PAD);
+  line(width-PAD, PAD, width-PAD, height-PAD);
+  line(PAD, height-PAD, width-PAD, height-PAD);
+  line(PAD, PAD, PAD, height-PAD);
 }
