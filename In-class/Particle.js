200287ecb567852fac87c03dc6be7f59edb07168
Elaine <ra1695@nyu.edu>
2017-04-11 19:40:37 +0800

    exploding balls, with memory optimization

--- In-class/Particle.js
+++ In-class/Particle.js
@@ -1,70 +1,44 @@
 "use strict";
 
-const C_GRAVITY = 5;
-
 class Particle {
-  constructor(x, y, m, vx, vy) {
+  constructor(x, y) {
     this.pos = createVector(x, y);
-    this.vel = createVector(vx, vy);
+    this.vel = createVector(random(-5, 5), random(-5, 5));
     this.acc = createVector(0, 0);
-    this.mass = m;
-    this.rad = 3 * this.mass;
+    this.rad = random(2, 5);
     this.color = color(255);
-  }
-  applyForce(f) {
-    f.div(this.mass);
-    this.acc.add(f);
-  }
-
-  applyAttraction(other) {
-    var distance = this.pos.dist(other.pos); // distance
-    var magnitude = (C_GRAVITY * this.mass * other.mass)/(distance * distance); // gravity
-    var force = p5.Vector.sub(other.pos, this.pos);
-    force.normalize();
-    force.mult(magnitude);
-    this.applyForce(force);
-}
-
-  checkCollision(other) {
-    var distance = this.pos.dist(other.pos);
-    var force;
-    if (distance < this.rad + other.rad){
-      force = p5.Vector.sub(other.pos, this.pos);
-
-      // other.vel.mult(0);
-      this.applyForce(force.mult(-1));
-      this.vel.mult(0);
-      //other.applyForce(force.mult(-1));
-    }
-
-  }
-  checkEdges() {
-    if (this.pos.x < 0) {
-      this.pos.x = 0;
-      this.vel.x = -this.vel.x;
-    } else if (this.pos.x > width) {
-      this.pos.x = width;
-      this.vel.x = -this.vel.x;
-    }
-    if (this.pos.y < 0) {
-      this.pos.y = 0;
-      this.vel.y = -this.vel.y;
-    } else if (this.pos.y > height) {
-      this.pos.y = height;
-      this.vel.y = -this.vel.y;
-    }
+    this.lifeSpan = 1;
+    this.lifeDecrease = 0.01;
+    this.isDone = false;
   }
   update() {
     this.vel.add(this.acc);
     this.pos.add(this.vel);
     this.acc.mult(0);
+
+    this.lifeSpan -= this.lifeDecrease;
+
+    if (this.lifeSpan < 0){
+      this.lifeSpan = 0;
+      this.isDone = true;
+    }
+    //this.vel.mult(0.98);
   }
   display() {
     push();
     translate(this.pos.x, this.pos.y);
     noStroke();
-    fill(255, 100);
-    ellipse(0, 0, this.rad * 2, this.rad * 2);
+    fill(255);
+    ellipse(0, 0, this.rad * 2 * this.lifeSpan, this.rad * 2 * this.lifeSpan);
     pop();
+  }
+
+  checkOutOfCanvas() {
+    if (this.pos.x < PAD || this.pos.x > width-PAD){
+      this.isDone = true;
+    }
+    if (this.pos.y < PAD || this.pos.y > height-PAD){
+      this.isDone = true;
+    }
   }
 }
