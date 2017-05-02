2850d8ed776dfb1584dde7468bcb2a4f9258fec5
Elaine <ra1695@nyu.edu>
2017-05-02 16:17:17 +0800

    moving recursive tree

--- In-class/sketch.js
+++ In-class/sketch.js
@@ -1,21 +1,45 @@
-var boids = [];
 
 function setup() {
   createCanvas(1200, 600);
-  for (var i = 0; i < 50; i++) {
-    boids.push(new Boid(width/2, height/2));
-  }
+  background(0);
+  frameRate(5);
 }
 
 function draw() {
   background(0);
-
-  for (var i = 0; i < boids.length; i++) {
-    var b = boids[i];
-    b.flock(boids);
-    b.cohesion(boids);
-    b.update();
-    b.checkEdges();
-    b.display();
+  for (var i=0; i<1; i++){
+    push();
+    translate(width/2, height);
+    branch(150);
+    pop();
   }
+  //noLoop();
+}
+
+function branch(len){
+  strokeWeight(len/10);
+
+  var g = map(len, 255, 50, 0, 150);
+  stroke(0, g, 0);
+  // stroke(255);
+  line(0, 0, 0, -len);
+
+  if (len > 15){
+    translate(0, -len);
+    var angle = PI/6 * random(0.8, 1.2);
+    var newlen = len * 4/5;
+    var noiseVal = noise(frameCount * 0.01) * 0.2;
+    push();
+    rotate(angle + noiseVal);
+    branch(newlen);
+    pop();
+
+    push();
+    rotate(-angle + noiseVal);
+    branch(newlen);
+    pop();
+
+    // rotate(-PI/6)
+  }
+
 }
