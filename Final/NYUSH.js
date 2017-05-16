cba5342913bb33ca9212c60029833e0b0014a22f
Elaine <ra1695@nyu.edu>
2017-05-16 13:51:29 +0800

    integrate two parts together

--- Final/NYUSH.js
+++ Final/NYUSH.js
@@ -5,6 +5,13 @@ var curp, curl; // current point and current letter
 var allowDrag;
 var apts;
 
+// for drawing background flow
+var t, v;
+var RESOLUTION = 20;
+var angles = [];
+var rows, cols;
+const NUM = 50;
+var vehicles = [];
 
 function setup(){
   createCanvas(1000, 600);
@@ -52,12 +59,69 @@ function setup(){
   apts.push(U);
 
   allowDrag = 0;
+
+  // background flow
+  rows = floor(height/RESOLUTION);
+  cols = floor(width/RESOLUTION);
+
+  for (var i=0; i<NUM; i++){
+    vehicles.push(new Vehicle(width-20, random(0, height)));
+  }
 }
 
 function draw(){
   background(0, 77, 102);
   noFill();
 
+  // flow field
+  noStroke();
+  var index = 0;
+  for (var c = 0; c < cols; c++){
+    for (var r = 0; r < rows; r++){
+
+      var x = c * RESOLUTION;
+      var y = r * RESOLUTION;
+
+      var xfreq = (x + frameCount) * 0.01;
+      var yfreq = (y + frameCount) * 0.01;
+      var amp = TWO_PI;
+      var val = noise(xfreq, yfreq) * amp;
+
+      angles[index] = val;
+      // var y = height/2 + val;
+      push();
+      translate(x, y);
+      noFill();
+
+      stroke(200);
+      // rect(0, 0, RESOLUTION, RESOLUTION);
+      // text(index, 5, 15);
+      rotate(val);
+      stroke(0);
+      // line(0, 0, RESOLUTION/2, 0);
+      pop();
+
+      index++;
+    }
+    var vCenter = createVector(width/2, height/2);
+    var vMouse = createVector(mouseX, mouseY);
+    var vector = p5.Vector.sub(vMouse, vCenter);
+
+    for (var i=0; i<vehicles.length; i++){
+      var v = vehicles[i];
+      var j = floor(v.pos.x / RESOLUTION);
+      var k = floor(v.pos.y / RESOLUTION);
+      noStroke();
+      // fill(0, 255, 0);
+      //ellipse(j*RESOLUTION, k*RESOLUTION, 10, 10);
+      // console.log(angles);
+      v.flow(angles[cols * k + j]);
+      v.update();
+      v.checkEdges();
+      v.display();
+    }
+  }
+  // NYU
   stroke(255);
   strokeWeight(1);
 
--- Final/Vehicle.js
+++ Final/Vehicle.js
@@ -1,58 +1,53 @@
 "use strict";
 
 class Vehicle {
-  constructor(x, y, r) {
+  constructor(x,y) {
     this.pos = createVector(x,y);
-    this.vel = createVector(random(-10, 10), random(-2, 2));
+    this.vel = createVector(0, random(-10, 10));
     this.acc = createVector();
     this.angle = 0;
     this.maxDesiredVelocity = 5;
     this.maxSteerForce = 0.1;
-
-    this.r = r;
-    this.froze = false;
-    this.lifeSpan = 500;
-    this.age = 0;
   }
   update() {
-    if (! this.froze){
-      this.vel.add(this.acc);
-      this.pos.add(this.vel);
-      this.vel.limit(1);
-      this.acc.mult(0);
-      this.angle = this.vel.heading();
-      this.age += 1;
-    }
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.vel.limit(1);
+    this.acc.mult(0);
+    this.angle = this.vel.heading();
   }
   flow(angle) {
-    if (! this.froze){
-      var desired = p5.Vector.fromAngle(angle);
-      desired.setMag(this.maxDesiredVelocity);
+    var desired = p5.Vector.fromAngle(angle);
+    desired.setMag(this.maxDesiredVelocity);
 
-      var steer = p5.Vector.sub(desired, this.vel);
-      steer.limit(this.maxSteerForce);
-      this.applyForce(steer);
-    }
+    var steer = p5.Vector.sub(desired, this.vel);
+    steer.limit(this.maxSteerForce);
+
+    this.applyForce(steer);
   }
   applyForce(force) {
     this.acc.add(force);
   }
-
-  freeze(){
-    this.acc = 0;
-    this.vel = 0;
-    this.froze = true;
-  }
-  decreaseR(){
-    this.r *= 0.995;
+  checkEdges() {
+    if (this.pos.x < 0) {
+      this.pos.x = width;
+    } else if (this.pos.x > width) {
+      this.pos.x = 0;
+    }
+    if (this.pos.y < 0) {
+      this.pos.y = height;
+    } else if (this.pos.y > height) {
+      this.pos.y = 0;
+    }
   }
   display(){
     push();
     translate(this.pos.x, this.pos.y);
     rotate(this.angle);
     noStroke();
-    fill(51, 66, 36);
-    ellipse(0, 0, this.r, this.r);
+    fill(255);
+    // triangle(0,0,-1,0.5,-1,-0.5);
+    ellipse(0, 0, 0.5, 0.5);
     pop();
   }
 }
